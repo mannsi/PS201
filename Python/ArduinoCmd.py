6608303fcea9aedbba0f115a3125bba9a75961a4
Mannsi <mannsi1983@gmail.com>
2013-08-31 14:19:00 +0000

    Changes to test script

--- Python/ArduinoCmd.py
+++ Python/ArduinoCmd.py
@@ -16,12 +16,33 @@ portNumberInt = int(portNumberString)
 
 try:
   connection = serial.Serial(portNumberInt, timeout = 2)
-  while True:
-    readValue = connection.read()
-    if readValue:
-      print("Got value: %s" % readValue)
+
+  command = int(input("USB connected. Now what motherfucker ?\n\t1 = Write voltage\n\t2 = Read voltage\n\t3 = Turn on output\n\t4 = Turn off output\n\t5 = Exit \n: "))
+  while command is not 5:
+    if command == 1:
+      voltage = int(input("What is the target voltage you want ? : "))
+
+      connection.write(0xC0)
+      connection.write(voltage)
+    elif command == 2:
+      print("Reading voltage")
+      connection.write(0xD0)
+      while connection.inWaiting() > 0:
+        voltageValue = connection.readline()
+      if voltageValue:
+        print("Got value: %s" % voltageValue)
+      else:
+        print("Got no value")
+    elif command == 3:
+      print("Not sure what should happen here")
+    elif command == 4:
+      print("Not sure what should happen here")
     else:
-      print("Got no value")
-    time.sleep(1)
+      print("Invalid input, dumbass")
+
+    command = int(input("Again ?\n\t1 = Write voltage\n\t2 = Read voltage\n\t3 = Turn on output\n\t4 = Turn off output\n\t5 = Exit \n: "))
+
 except Exception as ex:
-    print("Error!. Error message: %s" % ex)
+  print("Error!. Error message: %s" % ex)
+
+
