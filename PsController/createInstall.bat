717e6d2ef9719905efb8d240719ae6d9fec97469
Guðbjörn Einarsson <mannsi1983@gmail.com>
2014-03-09 15:14:30 +0000

    Moving install related files around

--- PsController/createInstall.bat
+++ PsController/createInstall.bat
@@ -1,7 +1,6 @@
 @echo off
-cd PsController
 python createExe.py build
-"C:\Program Files (x86)\Inno Setup 5\iscc" ..\PsController.iss
+"C:\Program Files (x86)\Inno Setup 5\iscc" PsController.iss
 RMDIR build /S /Q
 
 
