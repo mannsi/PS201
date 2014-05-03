894d19c9f1f77660b4270e115d7868d0f10752a6
Guðbjörn Einarsson <mannsi1983@gmail.com>
2014-05-03 16:22:10 +0200

    - Adding better support for icons. They were not being referenced correctly before - Removing redundant debugging code - Clearing buffer before sending to device to minimize streaming issues

--- Software/setup.py
+++ Software/setup.py
@@ -21,13 +21,14 @@ setup(
     , version="0.1"
     , description="PsController"
     , packages=find_packages()
+    , package_data={'PsController': ['Icons/*.gif']}
     , install_requires=[
         "APScheduler == 2.1.1",
         "crcmod == 1.7",
         "pyserial == 2.7"
     ]
     , scripts=["psControllerMain.py"]
-    , data_files=[("Icons", dataFiles)]
+    #, data_files=[("Icons", dataFiles)]
     , entry_points={
         "console_scripts": [
             "PsController = psControllerMain:run"
